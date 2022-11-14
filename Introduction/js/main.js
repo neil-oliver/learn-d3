becebd24dcc3981322a070462ba671b8192f2c43
neil-oliver <neiloliverphoto@gmail.com>
2022-11-14 12:38:57 -0500

    ready for session start

---
+++ Introduction/js/main.js
@@ -0,0 +1,7 @@
+let data = [
+    { gdpPercap: 5, lifeExp: 5, pop: 7, continent: 'Asia' },
+    { gdpPercap: 1, lifeExp: 4, pop: 1, continent: 'Europe' },
+    { gdpPercap: 4, lifeExp: 6, pop: 6, continent: 'Africa' },
+    { gdpPercap: 6, lifeExp: 8, pop: 5, continent: 'Americas' },
+    { gdpPercap: 3, lifeExp: 2, pop: 3, continent: 'Oceania' }
+]
